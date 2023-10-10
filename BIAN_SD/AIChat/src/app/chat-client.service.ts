0cb5fa075a172ff672741702e3c0a13f6e2d4423
sujoyghosal <sujoy.ghosal@gmail.com>
2023-10-10 12:07:33 +0530

    GenAI

---
+++ BIAN_SD/AIChat/src/app/chat-client.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core'
+import { ChatComponentComponent } from './chat-component/chat-component.component'
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpErrorResponse
+} from '@angular/common/http'
+import { Observable, throwError } from 'rxjs'
+import { catchError, retry } from 'rxjs/operators'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChatClientService {
+  private restUrl = 'http://localhost:3000/vectorsearch'
+  private httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  }
+  constructor (private httpClient: HttpClient) {}
+  getExpenseEntries (): Observable<any> {
+    return this.httpClient
+      .get(this.restUrl, this.httpOptions)
+      .pipe(retry(3), catchError(this.httpErrorHandler))
+  }
+
+  getExpenseEntry (id: number): Observable<any> {
+    return this.httpClient
+      .get(this.restUrl + '/' + id, this.httpOptions)
+      .pipe(retry(3), catchError(this.httpErrorHandler))
+  }
+
+  getVectorSearchResponse(data: string): Observable<string> {
+    return this.httpClient.post<string>(this.restUrl, data, this.httpOptions)
+    .pipe(
+       retry(3),
+       catchError(this.httpErrorHandler)
+    );
+ }
+
+  private httpErrorHandler (error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      console.error(
+        'A client side error occurs. The error message is ' + error.message
+      )
+    } else {
+      console.error(
+        'An error happened in server. The HTTP status code is ' +
+          error.status +
+          ' and the error returned is ' +
+          error.message
+      )
+    }
+
+    return throwError('Error occurred. Pleas try again')
+  }
+}
